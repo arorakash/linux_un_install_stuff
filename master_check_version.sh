f311e0a8cfa60fb004cee7c127d093662fb939b6
arorakash <akash.arora73elearn@gmail.com>
2017-08-01 17:55:29 +0530

    Giving more priority to python 3 than python 2

--- master_check_version.sh
+++ master_check_version.sh
@@ -5,5 +5,5 @@ source $DIRECTORY/check_version_setup_tools.sh
 source $DIRECTORY/check_version_pip_python.sh
 source $DIRECTORY/check_version_pip_python3.sh
 source $DIRECTORY/check_version_virtualenv.sh
-source $DIRECTORY/check_version_pip3_virtualenvwrapper.sh
 source $DIRECTORY/check_version_pip2_virtualenvwrapper.sh
+source $DIRECTORY/check_version_pip3_virtualenvwrapper.sh
--- master_install.sh
+++ master_install.sh
@@ -1,9 +1,9 @@
 DIRECTORY="/home/`logname`/Downloads"
-#source $DIRECTORY/install_python3.sh
 #source $DIRECTORY/install_python.sh
-source $DIRECTORY/install_pip_python3.sh
+#source $DIRECTORY/install_python3.sh
 source $DIRECTORY/install_pip_python.sh
-source $DIRECTORY/install_virtualenv_python3.sh
-source $DIRECTORY/install_virtualenvwrapper_python3.sh
+source $DIRECTORY/install_pip_python3.sh
 source $DIRECTORY/install_virtualenv_python.sh
 source $DIRECTORY/install_virtualenvwrapper_python.sh
+source $DIRECTORY/install_virtualenv_python3.sh
+source $DIRECTORY/install_virtualenvwrapper_python3.sh
