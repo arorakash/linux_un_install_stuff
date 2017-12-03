c5cd44429ac974c5140a2483866ec41cbab35a93
arorakash <akash.arora73elearn@gmail.com>
2017-12-03 11:46:12 +0530

    uninstall scripts to uninstall pycharm and intelliJ.
    Check version scripts not practical...yet!

---
+++ uninstall_intelliJ.sh
@@ -0,0 +1,3 @@
+sudo rm /usr/bin/intelliJ
+sudo sed -i '/intelliJ/d' ~/.profile 
+sudo rm -r /opt/itelliJ
---
+++ uninstall_pycharm.sh
@@ -0,0 +1,3 @@
+sudo rm /usr/bin/pycharm
+sudo sed -i '/pycharm/d' ~/.profile 
+sudo rm -r /opt/pycharm
